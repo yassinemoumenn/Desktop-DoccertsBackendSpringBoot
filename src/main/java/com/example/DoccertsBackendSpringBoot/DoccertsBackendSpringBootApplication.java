03f08f3d8bae877ad07ace4bdbb44301539abd19
yassine71-star <moumenyassin998@gmail.com>
2021-07-02 16:58:36 +0100

    :tada: first step

---
+++ src/main/java/com/example/DoccertsBackendSpringBoot/DoccertsBackendSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.DoccertsBackendSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoccertsBackendSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoccertsBackendSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/DoccertsBackendSpringBoot/DoccertsBackendSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DoccertsBackendSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoccertsBackendSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
